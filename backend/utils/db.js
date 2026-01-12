94a225591d5aff83e6322a9f8634290c72889fb5
Asmita Dahule <dahuleasmita@gmail.com>
2026-01-12 18:02:51 +0530

    job portal

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,16 +1,14 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-dotenv.config();
 
+// Connect to MongoDB
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
-    console.log("Connected to MongoDB");
-  } catch (err) {
-    console.log("Error connecting to MongoDB:", err.message);
-    process.exit(1); 
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected...");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+    process.exit(1);
   }
 };
-
 
 export default connectDB;
